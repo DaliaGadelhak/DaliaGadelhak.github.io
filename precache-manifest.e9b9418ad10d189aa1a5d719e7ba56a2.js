bbd9454d998117d17b3c85935c75521fa11cf5a1
DaliaGadelhak <52830281+DaliaGadelhak@users.noreply.github.com>
2020-08-22 11:26:41 +0100

    Add files via upload

---
+++ precache-manifest.e9b9418ad10d189aa1a5d719e7ba56a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb83d055574a5f60a274ae4b8199bff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2675f2f8916886ac672a",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a53ebcec1bfc3db3c41b",
+    "url": "/static/css/main.5f2870cc.chunk.css"
+  },
+  {
+    "revision": "2675f2f8916886ac672a",
+    "url": "/static/js/2.d06927ee.chunk.js"
+  },
+  {
+    "revision": "ed6d44160be3da7ea632241f05f7ba30",
+    "url": "/static/js/2.d06927ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a53ebcec1bfc3db3c41b",
+    "url": "/static/js/main.51e37a2e.chunk.js"
+  },
+  {
+    "revision": "43dae117c495751884c7",
+    "url": "/static/js/runtime-main.a25f3486.js"
+  }
+]);
